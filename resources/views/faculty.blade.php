7130b520d4d878e5dfe4ef059735f44b7b6cea0b
halukbuyukozkan <halukbuyukozkan@gmail.com>
2022-03-30 23:58:03 +0300

    Bootstrap cdn added, faculty table added

--- resources/views/faculty.blade.php
+++ resources/views/faculty.blade.php
@@ -9,7 +9,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
